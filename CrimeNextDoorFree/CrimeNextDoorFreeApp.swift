c5d114f037915a0e885b23accf1368404805cba7
Oliver Harvey <oliharvey@hotmail.com>
2022-01-23 12:24:17 +0000

    Initial Commit

---
+++ CrimeNextDoorFree/CrimeNextDoorFreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrimeNextDoorFreeApp.swift
+//  CrimeNextDoorFree
+//
+//  Created by Oliver Harvey on 23/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct CrimeNextDoorFreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
